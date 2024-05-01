eae7b08e12622ec3004d05b2055b851e8b2bba96
Clark Belen <clarkbelen04@gmail.com>
2024-05-02 01:14:28 +0800

    10th commit for testing

--- js/app.js
+++ js/app.js
@@ -18,34 +18,31 @@ function getScheduleDataFromURL() {
       startTime: convertTo24HourFormat(data.schedule.split(' ')[1].split('-')[0]),
       endTime: convertTo24HourFormat(data.schedule.split(' ')[1].split('-')[1]),
       location: data.sectionRoom,
-      private: data.lecLabUnits.split('/')[0] !== '0.0' ? 'TRUE' : 'FALSE'
+      private: 'TRUE'
     };
 
-    // Directly add the row to the table
-    automaticBuildRow(rowData);
+     // Add the row to the table using the new function
+     addScheduleRow(rowData);
   });
  }
- automaticBuildRow = function(data) {
-  const tbody = document.querySelector('#display > table > tbody');
-  const tr = document.createElement('tr');
-  tr.setAttribute('data-row', document.querySelectorAll('tbody tr').length);
+ function addScheduleRow(scheduleData) {
+  // Get the table body element in which you want to add row
+  let table = document.querySelector('#display > table > tbody');
  
-  // Assuming the order of data properties matches the order of columns in the table
-  Object.values(data).forEach((value, index) => {
-     const td = document.createElement('td');
-     td.setAttribute('data-col', index);
-     td.textContent = value;
-     tr.appendChild(td);
+  // Create row element
+  let row = table.insertRow(-1); // Adding at the end
+ 
+  // Assuming the order of scheduleData properties matches the order of columns in the table
+  Object.values(scheduleData).forEach((value, index) => {
+     let cell = row.insertCell(index);
+     cell.textContent = value;
   });
  
   // Add edit and delete buttons
-  const actionCell = document.createElement('td');
+  let actionCell = row.insertCell(-1);
   actionCell.innerHTML = `<span class="glyphicon glyphicon-edit btn btn-info btn-sm" style="margin:2px;" id="edit" data-toggle="tooltip" data-placement="top" title="Edit"></span>
   <span class="glyphicon glyphicon-trash btn btn-danger btn-sm" id="delete" data-toggle="tooltip" data-placement="top" title="Delete"></span>`;
-  tr.appendChild(actionCell);
- 
-  tbody.appendChild(tr);
- };
+ }
  
  
  // Convert time to 24-hour format if necessary
